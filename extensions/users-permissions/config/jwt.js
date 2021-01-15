5cf9a926596c72f530211dbcd85e71a0e444e9cb
kevin <kevin.jacque@alumnos.ucentral.cl>
2021-01-15 08:44:57 -0300

    make it better

--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b7468261-2fed-4327-a302-a170d05917f5'
-};
--- publicstrapi/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("/Users/kevinjuanjacquecastillo/Desktop/ripley/para-romper/build"));
-app.listen(2000,()=>{
-    console.log(express.static(__dirname.toString().replace("publicstrapi","build")))
-    console.log('escuchando por el puerto 2000')
-});
